b7bfb820fdfbbb85b7af4de77cdcee5ee3633771
lshvartser1959 <35888786+lshvartser1959@users.noreply.github.com>
2020-09-02 17:37:40 +0300

    Update adaptMain.py

--- _OnSet/adaptMain.py
+++ _OnSet/adaptMain.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import label_binarize
 
-from Adaptation._OnSet.BaseFunctions import *
+from BaseFunctions import *
 from Vent.simple_impute import simple_imputer
 
 
